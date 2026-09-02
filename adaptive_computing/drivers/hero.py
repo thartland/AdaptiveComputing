2844fb9083b9f69eae10509d50bdd82fccd2ff59
thartland <tucker.hartland@gmail.com>
2026-09-02 16:07:25 -0700

    fixing synchronization issue when using inline_manager/different fidelity levels

--- adaptive_computing/drivers/hero.py
+++ adaptive_computing/drivers/hero.py
@@ -74,6 +74,9 @@ class ActiveLoopDriverHero(ActiveLoopDriver):
         """
         x, fi_eval = self.get_next_sample()
         self.dataset.add_samples(x, i_fidelity=fi_eval)
+        if self.inline_manager is not None:
+            self.inline_manager.run_until_done(i_fidelity=fi_eval)
+            self.hero_wait_for_data_and_train()
         if self.retrain:
             self.surrogate.train(self.dataset)
 
